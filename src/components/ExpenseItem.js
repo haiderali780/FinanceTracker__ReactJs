72e556c62479ab6cb0318c776a54d38cc760eb1e
haiderali780 <muhammadhaiderali782002@gmail.com>
2022-12-24 13:13:48 -0800

    basic components/inetraction but components are static

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,26 @@
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "./Card";
+import React, { useState } from "react";
+
+function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const ClickHandler = () => {
+    setTitle("updated");
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+
+      <button onClick={ClickHandler}>Title change</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
