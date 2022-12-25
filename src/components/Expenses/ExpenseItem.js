ab8fff40132712e43d5ef8b654919936d7b779d2
haiderali780 <muhammadhaiderali782002@gmail.com>
2022-12-25 15:10:11 -0800

    update.expenseItemcomponent::comment the unnecessary code

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,21 +4,21 @@ import Card from "../UI/Card";
 import React, { useState } from "react";
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
+ // const [title, setTitle] = useState(props.title);
 
-  const ClickHandler = () => {
+  /*const ClickHandler = () => {
     setTitle("updated");
-  };
-
+  };*/
+//<button onClick={ClickHandler}>Title change</button>
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
 
-      <button onClick={ClickHandler}>Title change</button>
+      
     </Card>
   );
 }
